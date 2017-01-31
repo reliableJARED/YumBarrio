69d6f489426a254807d4a5ac1f815569dfce50f8
reliableJARED <jarednugent@gmail.com>
2017-01-30 21:24:34 -0500

    work on new logins

--- login_facebook.js
+++ login_facebook.js
@@ -8,11 +8,10 @@ https://github.com/brianc/node-postgres/wiki/Parameterized-queries-and-Prepared-
 
 var pg = require('pg');
 
-//https://github.com/jaredhanson/passport
+
 var passport = require('passport')
 
-//https://github.com/jaredhanson/passport-local/blob/3b9980f6467fd1203ecf8844fb017b5db2941606/lib/utils.js
-var LocalStrategy = require('passport-local').Strategy;
+var FacebookStrategy = require('passport-facebook').Strategy;
 
 var app = require('express')();
 var session = require('express-session');
@@ -67,6 +66,17 @@ const connectionString = {
 //and set a limit of maximum 10 idle
 var client = new pg.Pool(connectionString);
 
+
+ /*
+//MAKE A TABLE
+//http://dba.stackexchange.com/questions/68266/postgresql-datatype-for-email-address
+ //DON'T use email as PK because The issue is that the email address cannot be changed afterwards, because it's both a primary key & referenced as foreign key
+const query = client.query(
+  'CREATE TABLE logintest (id SERIAL PRIMARY KEY,  email VARCHAR(256) not null,password VARCHAR(256) not null)');
+  	
+query.on('end', () => { client.end(); });
+*/
+
  /*
 //ADD A ROW --SAFE
 const query = client.query(
@@ -80,7 +90,66 @@ app.get('/', function(request, response){
 	response.sendFile(__dirname+'/home.html');
 });
 
+ 
+//CREATE FILE: app/routes.js
+//*****EXPORT BELOW
+    // =====================================
+    // HOME PAGE (with login links) ========
+    // =====================================
+    /*
+app.get('/', function(request, response) {
+        response.sendFile(__dirname+'/home.html');
+    });
 
+app.post('/login', function(request, response) {
+		//	console.log(Object.keys(request));
+			console.log('input:',request.body.username);
+			findEmail({column:'email', data:request.body.username});
+			//var result = findEmail({column:'email', data:request.body.username});
+		//	console.log('result',result)
+      //  if(result === request.body.username)response.sendFile(__dirname+'/login.html');
+    });
+
+
+
+
+function findEmail(find) {
+	
+	//Database Connection Config
+	var DATABASE = new pg.Client({
+   	 user: environment.DB_USER,
+    	password: environment.DB_PASS,
+    	database: environment.DB_NAME,
+   	 port: 5432,
+    	host: environment.DB_HOST,
+    	ssl: true
+	});
+
+	//CONNECT TO THE DB
+	DATABASE.connect();
+	
+	//wrap query in promise: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise/then
+	const Qresult = new Promise(
+				function(resolve,reject){
+					resolve(DATABASE.query("SELECT email FROM logintest WHERE email=$1", [find.data]));
+					});
+		//http://stackoverflow.com/questions/22078839/installing-passportjs-with-postgresql		
+		//GOT WHAT WE WERE LOOKING FOR		
+		Qresult.then( (success) => {
+				console.log('found',success.rows[0].email)
+
+				DATABASE.end();
+     		   return success.rows[0].email;
+      	})
+      //SOMETHING WENT WRONG
+      .catch( (err) => {
+      		DATABASE.end();
+     		   log.error("/login ERROR: " + err);
+        		return 999;
+     	 });
+	return Qresult
+};
+*/
 //****************************************************************
 //		file:		USER.JS  
 //****************************************************************
@@ -102,7 +171,7 @@ var client = new pg.Pool(connectionString);
 
 function User(){
 */
-var User = function() {    
+function User() {    
 	 this.database_id;//SQL assigned Primary Key, our database assigns this
     this.email;
     this.password;
@@ -155,13 +224,8 @@ var User = function() {
 }
 
 User.findByEmail = function(email, callback){
-	
-	 // callback has 3 args we need to return:
-        // arg 1: err: false if there is no error
-        // arg 2: isAvailable: whether the email is available or not
-        // arg 3: this: the User object or null
-        
-    var isAvailable = false; //change to true if email NOT found in db
+
+    var isAvailable = false; //change to true if email not found in db
    
     console.log('looking for email in User.findByEmail',email);
     console.log('then call',callback);
@@ -172,32 +236,34 @@ User.findByEmail = function(email, callback){
       	  if(err){
          	   return callback(err, isAvailable, this);
         		}
-       		
-       		//user found
+       
         		if (result.rowCount === 1){
-            	 	  console.log(email + ' was found!');
+            	 console.log(email + ' was found!');
             	
             	 	  var user = new User();
                     user.email= result.rows[0]['email'];
                     user.password = result.rows[0]['password'];
                     user.database_id = result.rows[0]['id'];
-
-            	     done();//disconnect from db
-            	     
-            	     return callback(null,isAvailable, user);
+                    done();
+                   return callback(null,user);
+            	  done();//disconnect from db
+            	 return callback(false, user);
         		}
-        		//no error, but the email has no account
-        		if (result.rowCount === 0) {
+        		else {
             	isAvailable = true;
             	console.log(email + ' is available');
+            	//no error, but the email has no account
             	 done();//disconnect from db
-            	 return callback(null, isAvailable, null);
+            	 return callback(false, false);
         		}
-       
+        //the callback has 3 args:
+        // arg err: false if there is no error
+        // arg isAvailable: whether the email is available or not
+        // arg this: the User object;
 
         done();//disconnect from db
         
-       
+        //return callback(false, isAvailable, this);
    	 });
 	});
 };
@@ -295,80 +361,96 @@ var User            = require('../SomePath/user');
         });
     });
 
+    
+     // =========================================================================
+    // FACEBOOK ================================================================
     // =========================================================================
-    // LOCAL LOGIN =============================================================
-    // =========================================================================
-    // we are using named strategies since we have one for login and one for signup
-    // by default, if there was no name, it would just be called 'local'
+    passport.use('facebook-login', new FacebookStrategy({
 
-    passport.use('local-login',new LocalStrategy({
-    			//theses should be the KEY values from the login post request
-            usernameField : 'username',
-            passwordField : 'password',
-            passReqToCallback : true // allows us to pass back the entire request to the callback
+            // pull in our app id and secret from our auth.js file
+            clientID        : '954261538041538',
+            clientSecret    : '82c3c35b10b27be95c6aeb0a97558ac5',
+            callbackURL     : 'http://localhost:5000/login'
+
         },
-        function(req, email, password, done) { // callback with email and password from our form
 
-            // find a user whose email is the same as the forms email
-            // we are checking to see if the user trying to login already exists
-         	User.findByEmail(email, function(err, isAvailable,user) {
-                // if there are any errors, return the error before anything else
-                if (err) return done(err);
+        // facebook will send back the token and profile
+        function(token, refreshToken, profile, done) {
 
-                // if no user is found, return the message
-                if (isAvailable) return done(null, false, console.log('loginMessage: No user found, available.')); // req.flash is the way to set flashdata using connect-flash
+            // asynchronous
+            process.nextTick(function() {
 
-                // all is well, return successful user
-                return done(null, user);
+                // find the user in the database based on their facebook id
+                User.findOne({ 'facebook.id' : profile.id }, function(err, user) {
+
+                    // if there is an error, stop everything and return that
+                    // ie an error connecting to the database
+                    if (err)
+                        return done(err);
+
+                    // if the user is found, then log them in
+                    if (user) {
+                        return done(null, user); // user found, return that user
+                    } else {
+                        // if there is no user found with that facebook id, create them
+                        var newUser = new User();
+
+                        // set all of the facebook information in our user model
+                        newUser.facebook.id    = profile.id; // set the users facebook id
+                        newUser.facebook.token = token; // we will save the token that facebook provides to the user
+                        newUser.facebook.name  = profile.name.givenName + ' ' + profile.name.familyName; // look at the passport user profile to see how names are returned
+                        newUser.facebook.email = profile.emails[0].value; // facebook can return multiple emails so we'll take the first
+								
+								console.log(newUser)	
+								
+                        // save our user to the database
+                        newUser.save(function(err) {
+                            if (err)
+                                throw err;
+
+                            // if successful, return the new user
+                            return done(null, newUser);
+                        });
+                    }
+
+                });
             });
 
         }));
-	
-	return passport;
-    };
 
-
+		return passport;
+	}
 
 //***********************************************
 //		END file:     PASSPORT.JS
 //***********************************************
 
-//https://github.com/jaredhanson/passport
+
 passport = configPassport(passport);
 
 // required for passport
-app.use(session({ secret: 'iAMsoSECRET_1984' })); // session secret
+app.use(session({ secret: 'topSecretStuff411' })); // session secret
 app.use(passport.initialize());
 app.use(passport.session()); // persistent login sessions
 
 // process the login form
 // app.post('/login', do all our passport stuff here);
 // process the login form
-app.post('/login', passport.authenticate('local-login', {
+app.post('/login', passport.authenticate('facebook-login', {
         successRedirect : '/login', // redirect to the secure profile section
         failureRedirect : '/home', // redirect back to the signup page if there is an error
         failureFlash : false // allow flash messages
     }));
     
+    
  app.get('/login', isLoggedIn, function(req, res) {
-        console.log("REQUESTING",req.user);
+        console.log(req.user);
         res.sendFile(__dirname+'/login.html')
     });
-
-app.get('/logout', function(req, res) {
-	//Issue with destroying session
-	//http://stackoverflow.com/questions/13758207/why-is-passportjs-in-node-not-removing-session-on-logout
-        req.user.logOut();
-        req.session.destroy();
-        res.redirect('/home');
-    });
-        
- app.get('/home', function(req, res) {
-        console.log(req.user);
-        res.sendFile(__dirname+'/home.html')
-    });
- // middleware to make sure a user is logged in
+    
+ // route middleware to make sure a user is logged in
 function isLoggedIn(req, res, next) {
+
     // if user is authenticated in the session, carry on
     if (req.isAuthenticated()) {
         console.log('isLoggedin');
@@ -377,10 +459,36 @@ function isLoggedIn(req, res, next) {
     console.log('is not logged in');
 
     // if they aren't redirect them to the home page
-    res.redirect('/home');
+    res.redirect('/');
 }
 
    
-
+/*
+app.post('/login', 
+    passport.authenticate('local-login', { 
+    	failureRedirect: '/home', 
+    	failureFlash: true }),
+    function(req, res) {
+        res.redirect('/login');
+});
+*/
+/*
+app.post('/login', (request,response,next) => {
+		console.log(request.body)
+			passport.authenticate('local-login', (err, user, info) => {
+				console.log("AUTH")
+   		 if (err) { 
+   		 	console.log(err)
+   		 	response.sendFile(__dirname+'/home.html')}
+   		 if (!user) { 
+   		 	console.log('no user',err, user, info)
+   		 	response.sendFile(__dirname+'/home.html')}
+    		if (user) { 
+    			console.log('found user')
+    			response.sendFile(__dirname+'/login.html')
+    			}
+  		})(request, response, next);
+   });
+ */
  
  
